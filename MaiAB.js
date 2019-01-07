50cc861812afda2a81fa18e785567372fd2d9179
YabaiNyan <netmin@desuu.com>
2019-01-07 13:53:59 +0800

    Update MaiAB.js

--- MaiAB.js
+++ MaiAB.js
@@ -20,17 +20,14 @@ client.on('message', async message => {
     const command = message.content.toLowerCase().split(' ')[0]
     const arguments = message.content.split(' ')
     arguments.shift()
-    if (command == `${PREFIX}ping`) {
-        return message.reply('I\'m here!')
-    }
-    if (command == `${PREFIX}mal`) {
-        handleMalQuery(arguments.join(" "), message, true);
-    }
+    if (command == `${PREFIX}ping`) return message.reply('I\'m here!');
+    if (command == `${PREFIX}mal`) handleMalQuery(arguments.join(" "), message, true);
+
     var matches = message.content.match(/\<(.+?)\>/);
     if (matches) {
         var query = matches[1];
         var deletemessage = false;
-        if(matches[1].startsWith("@")||matches[1].startsWith(":")||matches[1].startsWith("#")) return
+        if(query.startsWith("@")||query.startsWith(":")||query.startsWith("#")) return
         if(matches[0] == message.content){
             deletemessage = true;
         }
@@ -49,38 +46,88 @@ function handleMalQuery(query, message, deletemessage){
         var year = data[0].payload.start_year;
         var score = data[0].payload.score;
         var status = data[0].payload.status;
-
-        message.channel.send({content: url, embed:{
-              title: name,
-              url: url,
-              color: 65508,
-              footer: {
-                  icon_url: message.author.avatarURL,
-                  text: message.author.username+"#"+message.author.discriminator
-              },
-              image: {
-                  url: image
-              },
-              timestamp: new Date(),
-              fields: [
-                  {
-                    name: "Score",
-                    value: score,
-                    inline: true
-                  },
-                  {
-                    name: "Year",
-                    value: year,
-                    inline: true
-                  },
-                  {
-                    name: "Status",
-                    value: status,
-                    inline: true
-                  }
-              ]
-        }})
-        if (deletemessage) message.delete().catch((err)=>{}); 
+        var synopsis;
+        var genres;
+        var studios;
+        var rating;
+        var ranked;
+        var popularity;
+        malScraper.getInfoFromURL(url)
+            .then((data) => {
+                image = data.picture;
+                genres = "`"+data.genres.join("` `")+"`";
+                studios = data.studios.join(", ");
+                rating = data.rating;
+                ranked = data.ranked;
+                popularity = data.popularity;
+                synopsis = toTweetLength(data.synopsis);
+                message.channel.send({content: url, embed:{
+                    title: name,
+                    url: url,
+                    color: 65508,
+                    footer: {
+                        icon_url: message.author.avatarURL,
+                        text: message.author.username+"#"+message.author.discriminator
+                    },
+                    image: {
+                        url: image
+                    },
+                    timestamp: new Date(),
+                    fields: [
+                        {
+                          name: "Synopsis",
+                          value: synopsis,
+                        },
+                        {
+                          name: "Genres",
+                          value: genres,
+                        },
+                        {
+                          name: "Studios",
+                          value: studios,
+                        },
+                        {
+                          name: "Score",
+                          value: score,
+                          inline: true
+                        },
+                        {
+                          name: "Ranked",
+                          value: ranked,
+                          inline: true
+                        },
+                        {
+                          name: "Popularity",
+                          value: popularity,
+                          inline: true
+                        },
+                        {
+                            name: "Rating",
+                            value: rating,
+                        },
+                        {
+                          name: "Year",
+                          value: year,
+                          inline: true
+                        },
+                        {
+                          name: "Status",
+                          value: status,
+                          inline: true
+                        }
+                    ]
+                }})
+                if (deletemessage) message.delete().catch((err)=>{}); 
+            })
+            .catch((err) => console.log(err))
     })
     .catch((err) => console.log(err))
+}
+
+function toTweetLength(input){
+    if (input.length <= 140) {
+        return input
+    } else {
+        return input.slice(0,140) + "..."
+    }
 }
